006411e3657d03654212cb0091cbe52fa761d45d
Vasylyna Pavelko <vasylyna.pavelko99@gmail.com>
2023-07-01 23:13:15 +0300

    Created shop page and single product page

--- script.js
+++ script.js
@@ -11,3 +11,14 @@ mobileBar.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
     navbar.classList.remove('active');
 })
+
+// Changing product image in single product page
+
+let mainImage = document.getElementById('main-image');
+const smallImage = document.getElementsByClassName('small-image');
+
+[...smallImage].forEach(image => {
+image.addEventListener('click', () => {
+    mainImage.src = image.src;
+})
+})
